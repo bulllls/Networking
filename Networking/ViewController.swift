4b2995f24d5b8f841cd319b8c99f92452e0a87c5
Ruslan <ruslanfilistovich@gmail.com>
2019-10-24 21:11:51 +0300

    03. Download image from the network

--- Networking/ViewController.swift
+++ Networking/ViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var dowloadLabel: UILabel!
+    @IBOutlet weak var dowloadButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        activityIndicator.isHidden = true
+        activityIndicator.hidesWhenStopped = true
     }
 
 
+    @IBAction func dowloadActionButton(_ sender: Any) {
+        
+        dowloadLabel.isHidden = true
+        dowloadButton.isEnabled = false
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+        
+        guard let url = URL(string: "https://applelives.com/wp-content/uploads/2016/03/iPhone-SE-11.jpeg") else { return }
+        
+        let session = URLSession.shared
+        session.dataTask(with: url) { (data, response, error) in
+            if let data = data, let imageSession = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    self.activityIndicator.stopAnimating()
+                    self.image.image = imageSession
+                }
+            }
+        }.resume()
+        
+    }
 }
 
