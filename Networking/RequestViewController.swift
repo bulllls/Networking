f8f64dce347aa8e393f970b286cfd5b68b81b4b1
Ruslan <ruslanfilistovich@gmail.com>
2019-10-27 15:25:11 +0300

    GET Request

--- Networking/RequestViewController.swift
+++ Networking/RequestViewController.swift
@@ -27,11 +27,26 @@ class RequestViewController: UIViewController {
     
     
     @IBAction func downloadAction(_ sender: Any) {
-        
-        
-        
     }
     @IBAction func getAction(_ sender: Any) {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        let session = URLSession.shared
+        session.dataTask(with: url) { (data, response, error) in
+            guard
+                let response = response,
+                let data = data
+            else { return }
+            print(response)
+            print(data)
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                print(json)
+            } catch {
+                print(error)
+            }
+            
+        }.resume()
     }
     @IBAction func postAction(_ sender: Any) {
     }
